f14a6c8ca8432c198dbae8643597f959354b66da
Gourav Bhadra <118394124+gouravbhadra2001@users.noreply.github.com>
2022-12-02 11:55:54 +0530

    Update main.dart

--- DropDownList/main.dart
+++ DropDownList/main.dart
@@ -2,167 +2,50 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        color: Colors.brown,
-        title: "Simple Interest Calculator",
-        home: SimpleInterestForm(),
-      )
+    MaterialApp(
+      title: "Custom_components: DropDownList",
+      home: DropDownList()
+    )
   );
 }
 
-class SimpleInterestForm extends StatefulWidget{
+class DropDownList extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
-    return SimpleInterestFormState();
+    return DropDownListState();
   }
 }
 
-class SimpleInterestFormState extends State<SimpleInterestForm>{
-  var principal;
-  var rate_of_interest;
-  var time;
-  var _currencies = ["Rupees", "Dollars", "Pound", "Euro"];
-  var current = "Rupees";
-  var display = '';
-  TextEditingController principalController = TextEditingController();
-  TextEditingController roiController = TextEditingController();
-  TextEditingController termController = TextEditingController();
+class DropDownListState extends State<DropDownList>{
+  var _currencies = ["None","Item1", "Item2", "Item3", "Item4"];
+  var current = "None";
+
   @override
+
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-          title: Text("Simple Interest Calculator"),
-          leading: Icon(Icons.currency_rupee),
+        title: Text("Custom DropDownList Example"),
       ),
       body: Container(
-        child: ListView(
-          children: <Widget>[
-            SizedBox(height: 20.0,),
-
-            TextField(
-              controller: principalController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  hintText: "Principle",
-                  labelText: "Principle",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0))
-              ),
-    ),
-            SizedBox(height: 10.0,),
-            TextField(
-              controller: roiController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  hintText: "Rate Of Interest",
-                  labelText: "ROI",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0)
-                )
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: termController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      hintText: "Terms",
-                      labelText: "Terms",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0)
-                      )
-                  ),
-                ),
-                ),
-                SizedBox(width: 20.0,),
-                Expanded(child: DropdownButton<String>(
-                    items: _currencies.map((String value){
-                      return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value)
-                      );
-                    }).toList(),
-                    value: current,
-                    onChanged: (var newValue){
-                      setState(() {
-                        current = newValue!;
-                      });
-                    }))
-              ],
-            ),
-            SizedBox(height: 10.0,),
-            Row(
-              children: <Widget>[
-                Expanded(child:
-                Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40.0),
-                    child:ElevatedButton(onPressed: () {
-                      setState((){
-                        this.display = calculate(current);
-                      });
-                    },
-                      child: Text("Calculate"),
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)
-                        )
-                      )),
-                    )),
-
-
-                Expanded(child:
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                  child: ElevatedButton(onPressed: () {
-                    setState(() {
-                      termController.text='';
-                      principalController.text='';
-                      roiController.text='';
-                      current=_currencies[0];
-                      display = '';
-                    });
-                  },
-                    child: Text("Reset"),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0)
-                          )
-                      )
-                  )),
-                )
-              ],
-            ),
-            SizedBox(height: 30.0,),
-            Center(child:
-            Padding(padding: EdgeInsets.all(60.0),
-              child: Text(this.display, style: TextStyle(
-              fontWeight: FontWeight.bold,
-                fontSize: 40.0
-            ),),))
-  ]
-    )
-    ));
+        child: Center(
+          child: DropdownButton<String>(
+              items: _currencies.map((String value){
+                return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value)
+                );
+              }).toList(),
+              value: current,
+              onChanged: (var newValue){
+                setState(() {
+                  current = newValue!;
+                });
+              }),
+        )
+      ),
+    );
   }
-  String calculate(String current){
-      double p = double.parse(principalController.text),
-      r = double.parse(roiController.text),
-      t = double.parse(termController.text);
-      double interest = (p*r*t)/100, amount = p+interest;
-      switch(current){
-        case "Pound":
-          return "Interest: £$interest and Amount: £$amount";
-        case "Dollars":
-          return "Interest: \$$interest and Amount: \$$amount" ;
-        case "Euro":
-          return "Interest: €$interest and Amount: €$amount";
-        default:
-          return "Interest: ₹$interest/- and Amount: ₹$amount/-";
-      }
-  }
+
 }
